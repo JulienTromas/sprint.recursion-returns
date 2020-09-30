386e08c5c16c7f8f53dca8a921ea1f37c3c7e617
Julien Tromas <julien.tromas@gmail.com>
2020-09-30 12:03:39 +0900

    Test commit
    Co-authored-by: alexdang1993374 <alexdang1993374@users.noreply.github.com>
    Co-authored-by: JulienTromas <julientromas@users.noreply.github.com>

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,5 +1,21 @@
-const getElementsByClassName = () => {
-  // YOUR CODE HERE
+const getElementsByClassName = (input) => {
+  const resultArray = [];
+
+  function check(element) {
+    if (element.className.includes(input)) {
+      resultArray.push(element);
+    }
+    if (element.children === undefined) {
+      return;
+    }
+    for (const child of element.children) {
+      check(child);
+    }
+  }
+
+  check(document.body);
+
+  return resultArray;
 };
 
 module.exports = { getElementsByClassName };
