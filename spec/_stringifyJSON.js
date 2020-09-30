7d7f8a294e46b7133c1d0e06d399d1ed42f8f7f3
Julien Tromas <julien.tromas@gmail.com>
2020-09-30 18:01:34 +0900

    Wrote stringifyJSON and some basic tests which it passes

--- spec/_stringifyJSON.js
+++ spec/_stringifyJSON.js
@@ -3,15 +3,24 @@ const { stringifyJSON } = require("../src/stringifyJSON");
 
 describe("stringifyJSON", () => {
   describe("basic stuff", () => {
-    it("should return an array as string", () => {
+    it.only("should return an array as string", () => {
       expect(stringifyJSON([])).to.equal("[]");
     });
   });
 
   // FIXME: Write your tests
-  describe("???", () => {
-    it("should return the right answer", () => {
-      expect("FIXME:").to.equal("Write tests");
+  describe("StringifyJSON", () => {
+    it.only("should return a string for a number", () => {
+      expect(typeof stringifyJSON(3)).to.equal("string");
+    });
+    it.only("should return a string for a boolean", () => {
+      expect(typeof stringifyJSON(true)).to.equal("string");
+    });
+    it.only("should return a stringified array with values inside", () => {
+      expect(stringifyJSON([3, "false", false])).to.equal("[3,false,false]");
+    });
+    it.only("should return a stringified object", () => {
+      expect(stringifyJSON({ x: 5, y: 6 })).to.equal("{ x: 5, y: 6 }");
     });
   });
 });
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,5 +1,30 @@
-const stringifyJSON = () => {
-  // YOUR CODE HERE
+const stringifyJSON = (input) => {
+  if (typeof input === "number" || typeof input === "boolean") {
+    return `${input}`;
+  } else if (
+    input === undefined ||
+    input instanceof Function ||
+    typeof input === "symbol"
+  ) {
+    return "null";
+  } else if (typeof input === "string") {
+    return `${input}`;
+  } else if (Array.isArray(input)) {
+    const arrayElements = [];
+    for (const element of input) {
+      arrayElements.push(stringifyJSON(element));
+    }
+    return `[${arrayElements}]`;
+  } else if (input instanceof Object) {
+    const objKeysArray = Object.keys(input);
+    const resultArrayOfKeys = [];
+    objKeysArray.forEach((key) => {
+      resultArrayOfKeys.push(
+        ` ${stringifyJSON(key)}: ${stringifyJSON(input[key])}`
+      );
+    });
+    return `{${resultArrayOfKeys} }`;
+  }
 };
 
 module.exports = { stringifyJSON };
