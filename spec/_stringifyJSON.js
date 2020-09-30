a62eec9b798a57bd41cf74435dab4a5d65144838
Julien Tromas <julien.tromas@gmail.com>
2020-09-30 22:29:55 +0900

    Removed the it.only in the tests of StringifyJSON

--- spec/_stringifyJSON.js
+++ spec/_stringifyJSON.js
@@ -3,23 +3,23 @@ const { stringifyJSON } = require("../src/stringifyJSON");
 
 describe("stringifyJSON", () => {
   describe("basic stuff", () => {
-    it.only("should return an array as string", () => {
+    it("should return an array as string", () => {
       expect(stringifyJSON([])).to.equal("[]");
     });
   });
 
   // FIXME: Write your tests
   describe("StringifyJSON", () => {
-    it.only("should return a string for a number", () => {
+    it("should return a string for a number", () => {
       expect(typeof stringifyJSON(3)).to.equal("string");
     });
-    it.only("should return a string for a boolean", () => {
+    it("should return a string for a boolean", () => {
       expect(typeof stringifyJSON(true)).to.equal("string");
     });
-    it.only("should return a stringified array with values inside", () => {
+    it("should return a stringified array with values inside", () => {
       expect(stringifyJSON([3, "false", false])).to.equal("[3,false,false]");
     });
-    it.only("should return a stringified object", () => {
+    it("should return a stringified object", () => {
       expect(stringifyJSON({ x: 5, y: 6 })).to.equal("{ x: 5, y: 6 }");
     });
   });
